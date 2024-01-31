a752862f9e74984edded55f7c18e6d5f154c5b83
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2024-01-31 23:34:40 +0900

    Benchmark for fft2

---
+++ fft/perf_test/PerfTest_FFT2.cpp
@@ -0,0 +1,123 @@
+#include <benchmark/benchmark.h>
+#include "Benchmark_Context.hpp"
+#include "PerfTest_FFT2.hpp"
+
+namespace KokkosFFTBenchmark {
+
+// 2D FFT on 2D View
+BENCHMARK(FFT2_2DView<float, Kokkos::LayoutLeft>)
+    ->ArgName("N")
+    ->RangeMultiplier(2)
+    ->Range(256, 4096)
+    ->UseManualTime()
+    ->Unit(benchmark::kMicrosecond);
+
+BENCHMARK(FFT2_2DView<float, Kokkos::LayoutRight>)
+    ->ArgName("N")
+    ->RangeMultiplier(2)
+    ->Range(256, 4096)
+    ->UseManualTime()
+    ->Unit(benchmark::kMicrosecond);
+
+BENCHMARK(FFT2_2DView<double, Kokkos::LayoutLeft>)
+    ->ArgName("N")
+    ->RangeMultiplier(2)
+    ->Range(256, 4096)
+    ->UseManualTime()
+    ->Unit(benchmark::kMicrosecond);
+
+BENCHMARK(FFT2_2DView<double, Kokkos::LayoutRight>)
+    ->ArgName("N")
+    ->RangeMultiplier(2)
+    ->Range(256, 4096)
+    ->UseManualTime()
+    ->Unit(benchmark::kMicrosecond);
+
+// 2D IFFT on 2D View
+BENCHMARK(IFFT2_2DView<float, Kokkos::LayoutLeft>)
+    ->ArgName("N")
+    ->RangeMultiplier(2)
+    ->Range(256, 4096)
+    ->UseManualTime()
+    ->Unit(benchmark::kMicrosecond);
+
+BENCHMARK(IFFT2_2DView<float, Kokkos::LayoutRight>)
+    ->ArgName("N")
+    ->RangeMultiplier(2)
+    ->Range(256, 4096)
+    ->UseManualTime()
+    ->Unit(benchmark::kMicrosecond);
+
+BENCHMARK(IFFT2_2DView<double, Kokkos::LayoutLeft>)
+    ->ArgName("N")
+    ->RangeMultiplier(2)
+    ->Range(256, 4096)
+    ->UseManualTime()
+    ->Unit(benchmark::kMicrosecond);
+
+BENCHMARK(IFFT2_2DView<double, Kokkos::LayoutRight>)
+    ->ArgName("N")
+    ->RangeMultiplier(2)
+    ->Range(256, 4096)
+    ->UseManualTime()
+    ->Unit(benchmark::kMicrosecond);
+
+// 2D RFFT on 2D View
+BENCHMARK(RFFT2_2DView<float, Kokkos::LayoutLeft>)
+    ->ArgName("N")
+    ->RangeMultiplier(2)
+    ->Range(256, 4096)
+    ->UseManualTime()
+    ->Unit(benchmark::kMicrosecond);
+
+BENCHMARK(RFFT2_2DView<float, Kokkos::LayoutRight>)
+    ->ArgName("N")
+    ->RangeMultiplier(2)
+    ->Range(256, 4096)
+    ->UseManualTime()
+    ->Unit(benchmark::kMicrosecond);
+
+BENCHMARK(RFFT2_2DView<double, Kokkos::LayoutLeft>)
+    ->ArgName("N")
+    ->RangeMultiplier(2)
+    ->Range(256, 4096)
+    ->UseManualTime()
+    ->Unit(benchmark::kMicrosecond);
+
+BENCHMARK(RFFT2_2DView<double, Kokkos::LayoutRight>)
+    ->ArgName("N")
+    ->RangeMultiplier(2)
+    ->Range(256, 4096)
+    ->UseManualTime()
+    ->Unit(benchmark::kMicrosecond);
+
+// 2D IRFFT on 2D View
+BENCHMARK(IRFFT2_2DView<float, Kokkos::LayoutLeft>)
+    ->ArgName("N")
+    ->RangeMultiplier(2)
+    ->Range(256, 4096)
+    ->UseManualTime()
+    ->Unit(benchmark::kMicrosecond);
+
+BENCHMARK(IRFFT2_2DView<float, Kokkos::LayoutRight>)
+    ->ArgName("N")
+    ->RangeMultiplier(2)
+    ->Range(256, 4096)
+    ->UseManualTime()
+    ->Unit(benchmark::kMicrosecond);
+
+BENCHMARK(IRFFT2_2DView<double, Kokkos::LayoutLeft>)
+    ->ArgName("N")
+    ->RangeMultiplier(2)
+    ->Range(256, 4096)
+    ->UseManualTime()
+    ->Unit(benchmark::kMicrosecond);
+
+BENCHMARK(IRFFT2_2DView<double, Kokkos::LayoutRight>)
+    ->ArgName("N")
+    ->RangeMultiplier(2)
+    ->Range(256, 4096)
+    ->UseManualTime()
+    ->Unit(benchmark::kMicrosecond);
+
+}  // namespace KokkosFFTBenchmark
---
+++ fft/perf_test/PerfTest_FFT2.hpp
@@ -0,0 +1,104 @@
+#ifndef KOKKOSFFT_PERFTEST_FFT2_HPP
+#define KOKKOSFFT_PERFTEST_FFT2_HPP
+
+#include <Kokkos_Core.hpp>
+#include <KokkosFFT.hpp>
+#include <benchmark/benchmark.h>
+#include "Benchmark_Context.hpp"
+
+using execution_space = Kokkos::DefaultExecutionSpace;
+using axis_type = KokkosFFT::axis_type<2>;
+
+namespace KokkosFFTBenchmark {
+
+template <typename InViewType, typename OutViewType>
+void fft2(const InViewType& in, OutViewType& out, benchmark::State& state) {
+  for (auto _ : state) {
+    Kokkos::fence();
+    Kokkos::Timer timer;
+    KokkosFFT::fft2(execution_space(), in, out);
+    KokkosFFTBenchmark::report_results(state, in, out, timer.seconds());
+  }
+}
+
+template <typename InViewType, typename OutViewType>
+void ifft2(const InViewType& in, OutViewType& out, benchmark::State& state) {
+  for (auto _ : state) {
+    Kokkos::fence();
+    Kokkos::Timer timer;
+    KokkosFFT::ifft2(execution_space(), in, out);
+    KokkosFFTBenchmark::report_results(state, in, out, timer.seconds());
+  }
+}
+
+template <typename InViewType, typename OutViewType>
+void rfft2(const InViewType& in, OutViewType& out, benchmark::State& state) {
+  for (auto _ : state) {
+    Kokkos::fence();
+    Kokkos::Timer timer;
+    KokkosFFT::rfft2(execution_space(), in, out);
+    KokkosFFTBenchmark::report_results(state, in, out, timer.seconds());
+  }
+}
+
+template <typename InViewType, typename OutViewType>
+void irfft2(const InViewType& in, OutViewType& out, benchmark::State& state) {
+  for (auto _ : state) {
+    Kokkos::fence();
+    Kokkos::Timer timer;
+    KokkosFFT::irfft2(execution_space(), in, out);
+    KokkosFFTBenchmark::report_results(state, in, out, timer.seconds());
+  }
+}
+
+template <typename T, typename LayoutType>
+static void FFT2_2DView(benchmark::State& state) {
+  using ComplexView2DType =
+      Kokkos::View<Kokkos::complex<T>**, LayoutType, execution_space>;
+
+  const int n = state.range(0);
+  ComplexView2DType x("x", n, n), x_hat("x_hat", n, n);
+
+  fft2(x, x_hat, state);
+}
+
+template <typename T, typename LayoutType>
+static void IFFT2_2DView(benchmark::State& state) {
+  using ComplexView2DType =
+      Kokkos::View<Kokkos::complex<T>**, LayoutType, execution_space>;
+
+  const int n = state.range(0);
+  ComplexView2DType x("x", n, n), x_hat("x_hat", n, n);
+
+  ifft2(x, x_hat, state);
+}
+
+template <typename T, typename LayoutType>
+static void RFFT2_2DView(benchmark::State& state) {
+  using RealView2DType = Kokkos::View<T**, LayoutType, execution_space>;
+  using ComplexView2DType =
+      Kokkos::View<Kokkos::complex<T>**, LayoutType, execution_space>;
+
+  const int n = state.range(0);
+  RealView2DType x("x", n, n);
+  ComplexView2DType x_hat("x_hat", n, n / 2 + 1);
+
+  rfft2(x, x_hat, state);
+}
+
+template <typename T, typename LayoutType>
+static void IRFFT2_2DView(benchmark::State& state) {
+  using RealView2DType = Kokkos::View<T**, LayoutType, execution_space>;
+  using ComplexView2DType =
+      Kokkos::View<Kokkos::complex<T>**, LayoutType, execution_space>;
+
+  const int n = state.range(0);
+  ComplexView2DType x("x", n, n / 2 + 1);
+  RealView2DType x_hat("x_hat", n, n);
+
+  irfft2(x, x_hat, state);
+}
+
+}  // namespace KokkosFFTBenchmark
+
+#endif
