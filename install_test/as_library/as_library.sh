902216ad817d4c30168b2b535e1adafe1f100a3e
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2024-02-21 04:42:23 +0900

    fix script

--- install_test/as_library/as_library.sh
+++ install_test/as_library/as_library.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 args=$#
-WK_DIR=/tmp/kokkos_fft_as_library
-PWD=$(pwd)
+WK_DIR=$(pwd)
 ROOT_DIR=$1
 BUILD_TYPE=$2
 C_COMPILER=$3
@@ -17,10 +16,7 @@ export KOKKOS_BUILD_DIR=build_Kokkos
 export KOKKOSFFT_INSTALL_PREFIX=${ROOT_DIR}/usr/local/kokkosFFT
 export KOKKOSFFT_BUILD_DIR=build_KokkosFFT
 
-export EXAMPLE_BUILD_DIR=build_example_${TARGET}
-
-# Make work directory
-mkdir -p ${WK_DIR}
+export EXAMPLE_BUILD_DIR=build_example
 
 # Install Kokkos
 # Get Kokkos from github repo and build
@@ -32,7 +28,7 @@ cmake -DCMAKE_INSTALL_PREFIX=${KOKKOS_INSTALL_PREFIX} \
       -DCMAKE_C_COMPILER=${C_COMPILER} \
       -DCMAKE_CXX_COMPILER=${CXX_COMPILER} \
       -DCMAKE_CXX_STANDARD=17 \
-      ${BACKEND_FLAG}
+      ${BACKEND_FLAG} kokkos
       
 cmake --build . -j 8
 cmake --install .
@@ -42,7 +38,7 @@ cd ${WK_DIR}
 mkdir ${KOKKOSFFT_BUILD_DIR} && cd ${KOKKOSFFT_BUILD_DIR}
 cmake -DCMAKE_PREFIX_PATH=${KOKKOS_INSTALL_PREFIX} \
       -DCMAKE_INSTALL_PREFIX=${KOKKOSFFT_INSTALL_PREFIX} \
-      ${TARGET_FLAG}
+      ${TARGET_FLAG} ..
 
 cmake --build . -j 8
 cmake --install .
@@ -51,7 +47,7 @@ cmake --install .
 # Build KokkosFFT code using installed KokkosFFT
 cd ${WK_DIR}
 mkdir ${EXAMPLE_BUILD_DIR} && cd ${EXAMPLE_BUILD_DIR}
-cmake -DCMAKE_PREFIX_PATH="${KOKKOS_INSTALL_PREFIX};${KOKKOSFFT_INSTALL_PREFIX}" ../build_test/as_library
+cmake -DCMAKE_PREFIX_PATH="${KOKKOS_INSTALL_PREFIX};${KOKKOSFFT_INSTALL_PREFIX}" ../install_test/as_library
 cmake --build . -j 8
 
 if [ $? -eq 0 ]; then
