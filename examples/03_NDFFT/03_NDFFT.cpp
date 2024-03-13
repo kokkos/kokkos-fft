63ad0bf5a7cbd76dcc7806b91e7e502b101f0350
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2024-03-13 23:46:51 +0900

    use fence on execution space

--- examples/03_NDFFT/03_NDFFT.cpp
+++ examples/03_NDFFT/03_NDFFT.cpp
@@ -23,30 +23,31 @@ int main(int argc, char* argv[]) {
     View3D<Kokkos::complex<double> > xc2c_inv("xc2c_inv", n0, n1, n2);
 
     Kokkos::Random_XorShift64_Pool<> random_pool(12345);
-    Kokkos::fill_random(xc2c, random_pool, I);
-    Kokkos::fence();
+    execution_space exec;
+    Kokkos::fill_random(exec, xc2c, random_pool, I);
+    exec.fence();
 
-    KokkosFFT::fftn(execution_space(), xc2c, xc2c_hat);
-    KokkosFFT::ifftn(execution_space(), xc2c_hat, xc2c_inv);
-    Kokkos::fence();
+    KokkosFFT::fftn(exec, xc2c, xc2c_hat);
+    KokkosFFT::ifftn(exec, xc2c_hat, xc2c_inv);
+    exec.fence();
 
     // 3D R2C FFT
     View3D<double> xr2c("xr2c", n0, n1, n2);
     View3D<Kokkos::complex<double> > xr2c_hat("xr2c_hat", n0, n1, n2 / 2 + 1);
-    Kokkos::fill_random(xr2c, random_pool, 1);
-    Kokkos::fence();
+    Kokkos::fill_random(exec, xr2c, random_pool, 1);
+    exec.fence();
 
-    KokkosFFT::rfftn(execution_space(), xr2c, xr2c_hat);
-    Kokkos::fence();
+    KokkosFFT::rfftn(exec, xr2c, xr2c_hat);
+    exec.fence();
 
     // 3D C2R FFT
     View3D<Kokkos::complex<double> > xc2r("xr2c_hat", n0, n1, n2 / 2 + 1);
     View3D<double> xc2r_hat("xc2r", n0, n1, n2);
-    Kokkos::fill_random(xc2r, random_pool, I);
-    Kokkos::fence();
+    Kokkos::fill_random(exec, xc2r, random_pool, I);
+    exec.fence();
 
-    KokkosFFT::irfftn(execution_space(), xc2r, xc2r_hat);
-    Kokkos::fence();
+    KokkosFFT::irfftn(exec, xc2r, xc2r_hat);
+    exec.fence();
   }
   Kokkos::finalize();
 
