46e952ff626e6b6c2110b1791eca9cfb96633ca6
Yuuichi Asahi <yuuichi.asahi@cea.fr>
2024-08-20 10:38:49 +0200

    Improve to print details of tpls in CMake

--- cmake/KokkosFFT_tpls.cmake
+++ cmake/KokkosFFT_tpls.cmake
@@ -2,34 +2,51 @@
 #
 # SPDX-License-Identifier: MIT OR Apache-2.0 WITH LLVM-exception
 
-function(get_tpls_list tpls_list)
+function(get_tpls_list tpls_list backend_list)
+    # In order to display Tpls in the Kokkos order
+    # Device Parallel:
+    # Host Parallel:
+    # Host Serial:
     if(Kokkos_ENABLE_CUDA)
         list(APPEND tpls_list CUFFT)
+        list(APPEND backend_list "Device Parallel")
     elseif(Kokkos_ENABLE_HIP)
         if(KokkosFFT_ENABLE_ROCFFT)
             list(APPEND tpls_list ROCFFT)
         else()
             list(APPEND tpls_list HIPFFT)
         endif()
+        list(APPEND backend_list "Device Parallel")
     elseif(Kokkos_ENABLE_SYCL)
         list(APPEND tpls_list ONEMKL)
+        list(APPEND backend_list "Device Parallel")
     elseif(Kokkos_ENABLE_OPENMP)
         list(APPEND tpls_list FFTW_OPENMP)
+        list(APPEND backend_list "Host Parallel")
     elseif(Kokkos_ENABLE_THREADS)
         list(APPEND tpls_list FFTW_THREADS)
-    elseif(Kokkos_ENABLE_SERIAL)
-        list(APPEND tpls_list FFTW)
+        list(APPEND backend_list "Host Parallel")
     endif()
 
     if(KokkosFFT_ENABLE_HOST_AND_DEVICE)
-        if(Kokkos_ENABLE_OPENMP)
+        # Check if TPL is already included
+        list(FIND tpls_list "FFTW_OPENMP" idx_FFTW_OPENMP)
+        list(FIND tpls_list "FFTW_THREADS" idx_FFTW_THREADS)
+        if(Kokkos_ENABLE_OPENMP AND (idx_FFTW_OPENMP EQUAL -1))
             list(APPEND tpls_list FFTW_OPENMP)
-        elseif(Kokkos_ENABLE_THREADS)
+            list(APPEND backend_list "Host Parallel")
+        elseif(Kokkos_ENABLE_THREADS AND (idx_FFTW_THREADS EQUAL -1))
             list(APPEND tpls_list FFTW_THREADS)
-        else()
-            list(APPEND tpls_list FFTW)
+            list(APPEND backend_list "Host Parallel")
         endif()
     endif()
 
+    if(Kokkos_ENABLE_SERIAL)
+        list(APPEND tpls_list FFTW_SERIAL)
+        list(APPEND backend_list "  Host Serial")
+    endif()
+
     set(${tpls_list} ${${tpls_list}} PARENT_SCOPE)
+    set(${backend_list} ${${backend_list}} PARENT_SCOPE)
 endfunction()
+
