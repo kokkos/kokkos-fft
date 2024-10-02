e375f2c1b16a3b7692d9230650f63583865d5382
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2024-10-02 20:22:26 +0900

    fix: is_out_of_range_value_included function

--- common/src/KokkosFFT_utils.hpp
+++ common/src/KokkosFFT_utils.hpp
@@ -75,11 +75,13 @@ bool is_out_of_range_value_included(const ContainerType& values, IntType max) {
   static_assert(std::is_same_v<value_type, IntType>,
                 "is_out_of_range_value_included: Container value type must "
                 "match IntType");
-  bool is_included = false;
-  for (auto value : values) {
-    is_included = value >= max;
+  const auto [vmin, vmax] = std::minmax_element(values.begin(), values.end());
+  if constexpr (std::is_signed_v<value_type>) {
+    KOKKOSFFT_THROW_IF(
+        *vmin < 0,
+        "is_out_of_range_value_included: values must be non-negative");
   }
-  return is_included;
+  return *vmax >= max;
 }
 
 template <
