61c38265ef7c9c9bdb18c161f0be065c476c5268
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2024-10-15 10:56:28 +0900

    Align to_array implementation with Kokkos implementation

--- common/src/KokkosFFT_utils.hpp
+++ common/src/KokkosFFT_utils.hpp
@@ -215,24 +215,24 @@ Layout create_layout(const std::array<int, N>& extents) {
   return layout;
 }
 
-template <class T, std::size_t N, std::size_t... Is>
-constexpr Kokkos::Array<T, N> to_array_lvalue_helper(
-    const std::array<T, N>& a, std::index_sequence<Is...>) {
+template <typename T, std::size_t N, std::size_t... Is>
+constexpr Kokkos::Array<std::remove_cv_t<T>, N> to_array_lvalue(
+    std::array<T, N>& a, std::index_sequence<Is...>) {
   return {{a[Is]...}};
 }
-template <class T, std::size_t N, std::size_t... Is>
-constexpr Kokkos::Array<T, N> to_array_rvalue_helper(
+template <typename T, std::size_t N, std::size_t... Is>
+constexpr Kokkos::Array<std::remove_cv_t<T>, N> to_array_rvalue(
     std::array<T, N>&& a, std::index_sequence<Is...>) {
   return {{std::move(a[Is])...}};
 }
 
-template <class T, std::size_t N>
-constexpr Kokkos::Array<T, N> to_array(const std::array<T, N>& a) {
-  return to_array_lvalue_helper(a, std::make_index_sequence<N>());
+template <typename T, std::size_t N>
+constexpr Kokkos::Array<T, N> to_array(std::array<T, N>& a) {
+  return to_array_lvalue(a, std::make_index_sequence<N>());
 }
-template <class T, std::size_t N>
+template <typename T, std::size_t N>
 constexpr Kokkos::Array<T, N> to_array(std::array<T, N>&& a) {
-  return to_array_rvalue_helper(std::move(a), std::make_index_sequence<N>());
+  return to_array_rvalue(std::move(a), std::make_index_sequence<N>());
 }
 
 }  // namespace Impl
