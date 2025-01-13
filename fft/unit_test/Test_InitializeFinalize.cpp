ae958f46bea27eb33a6f00e2d64bb5c7f46c2584
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2025-01-13 16:53:48 +0900

    Add a missing LICENCE

--- fft/unit_test/Test_InitializeFinalize.cpp
+++ fft/unit_test/Test_InitializeFinalize.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: (C) The kokkos-fft development team, see COPYRIGHT.md file
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0 WITH LLVM-exception
+
 #include <gtest/gtest.h>
 #include <Kokkos_Core.hpp>
 #include "KokkosFFT_Core.hpp"
