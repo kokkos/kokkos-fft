80703ffe27a7ca71f2c648c23807acfc06550317
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2025-01-13 16:45:36 +0900

    Add KokkosFFT::initialize and KokkosFFT::finalize

---
+++ fft/unit_test/Test_MainNoInit.cpp
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: (C) 2022 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+// SPDX-FileCopyrightText: Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains certain rights in this software.
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <gtest/gtest.h>
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
