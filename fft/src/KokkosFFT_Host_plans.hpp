76f04f52bd9ff7e8c06ae6cf8f96407d0ee19973
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2025-01-15 17:49:51 +0900

    Add maybe unused for host plans

--- fft/src/KokkosFFT_Host_plans.hpp
+++ fft/src/KokkosFFT_Host_plans.hpp
@@ -18,7 +18,7 @@ template <typename ExecutionSpace, typename T,
           std::enable_if_t<is_AnyHostSpace_v<ExecutionSpace>, std::nullptr_t> =
               nullptr>
 void setup() {
-  static bool once = [] {
+  [[maybe_unused]] static bool once = [] {
     if (!(Kokkos::is_initialized() || Kokkos::is_finalized())) {
       Kokkos::abort(
           "Error: KokkosFFT APIs must not be called before initializing "
