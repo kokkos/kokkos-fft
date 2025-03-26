e2a35d1e3c480d4fad346376212f139027f19e31
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2025-03-26 17:02:56 +0900

    Add unit-testing tools

---
+++ testing/src/KokkosFFT_Testing.hpp
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: (C) The kokkos-fft development team, see COPYRIGHT.md file
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0 WITH LLVM-exception
+
+#ifndef KOKKOSFFT_TESTING_HPP
+#define KOKKOSFFT_TESTING_HPP
+
+#include "KokkosFFT_Concepts.hpp"
+#include "KokkosFFT_Allclose.hpp"
+
+#endif
