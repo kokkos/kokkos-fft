1fabd9cf7329fe6214dd8d5a56423cebb9fa1279
yasahi-hpc <57478230+yasahi-hpc@users.noreply.github.com>
2025-04-03 19:39:04 +0200

    Cleanup implementation details (#235)
    * Do not call fft/ifft inside rfft/irfft for better profiling
    
    * fix: the shape of view in inplace example
    
    * fix typo: Unmanged should be Unmanaged
    
    ---------
    
    Co-authored-by: Yuuichi Asahi <y.asahi@nr.titech.ac.jp>

--- fft/src/KokkosFFT_SYCL_transform.hpp
+++ fft/src/KokkosFFT_SYCL_transform.hpp
@@ -11,45 +11,45 @@
 
 namespace KokkosFFT {
 namespace Impl {
-template <typename PlanType, typename... Args>
+template <typename PlanType>
 void exec_plan(PlanType& plan, float* idata, std::complex<float>* odata,
-               int /*direction*/, Args...) {
+               int /*direction*/) {
   Kokkos::Profiling::ScopedRegion region(
       "KokkosFFT::exec_plan[TPL_oneMKLExecR2C]");
   oneapi::mkl::dft::compute_forward(plan, idata,
                                     reinterpret_cast<float*>(odata));
 }
 
-template <typename PlanType, typename... Args>
+template <typename PlanType>
 void exec_plan(PlanType& plan, double* idata, std::complex<double>* odata,
-               int /*direction*/, Args...) {
+               int /*direction*/) {
   Kokkos::Profiling::ScopedRegion region(
       "KokkosFFT::exec_plan[TPL_oneMKLExecD2Z]");
   oneapi::mkl::dft::compute_forward(plan, idata,
                                     reinterpret_cast<double*>(odata));
 }
 
-template <typename PlanType, typename... Args>
+template <typename PlanType>
 void exec_plan(PlanType& plan, std::complex<float>* idata, float* odata,
-               int /*direction*/, Args...) {
+               int /*direction*/) {
   Kokkos::Profiling::ScopedRegion region(
       "KokkosFFT::exec_plan[TPL_oneMKLExecC2R]");
   oneapi::mkl::dft::compute_backward(plan, reinterpret_cast<float*>(idata),
                                      odata);
 }
 
-template <typename PlanType, typename... Args>
+template <typename PlanType>
 void exec_plan(PlanType& plan, std::complex<double>* idata, double* odata,
-               int /*direction*/, Args...) {
+               int /*direction*/) {
   Kokkos::Profiling::ScopedRegion region(
       "KokkosFFT::exec_plan[TPL_oneMKLExecZ2D]");
   oneapi::mkl::dft::compute_backward(plan, reinterpret_cast<double*>(idata),
                                      odata);
 }
 
-template <typename PlanType, typename... Args>
+template <typename PlanType>
 void exec_plan(PlanType& plan, std::complex<float>* idata,
-               std::complex<float>* odata, int direction, Args...) {
+               std::complex<float>* odata, int direction) {
   Kokkos::Profiling::ScopedRegion region(
       "KokkosFFT::exec_plan[TPL_oneMKLExecC2C]");
   if (direction == 1) {
@@ -59,9 +59,9 @@ void exec_plan(PlanType& plan, std::complex<float>* idata,
   }
 }
 
-template <typename PlanType, typename... Args>
+template <typename PlanType>
 void exec_plan(PlanType& plan, std::complex<double>* idata,
-               std::complex<double>* odata, int direction, Args...) {
+               std::complex<double>* odata, int direction) {
   Kokkos::Profiling::ScopedRegion region(
       "KokkosFFT::exec_plan[TPL_oneMKLExecZ2Z]");
   if (direction == 1) {
