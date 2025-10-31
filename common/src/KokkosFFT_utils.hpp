5388b5a8da93b4e32ba77494819af3c37bf839df
yasahi <yasahi@login6.head.adastra.cines.fr>
2025-10-31 09:09:49 +0100

    Rename IntType to IndexType

--- common/src/KokkosFFT_utils.hpp
+++ common/src/KokkosFFT_utils.hpp
@@ -150,15 +150,15 @@ bool are_valid_axes(const ViewType& /*view*/,
 /// \brief Check if transpose is needed or not
 /// If a map is contiguous and in ascending order (e.g. {0, 1, 2}),
 /// we do not need transpose
-/// \tparam IntType The integer type used for map
+/// \tparam IndexType The integer type used for map
 /// \tparam DIM The dimensionality of the axes
 ///
 /// \param[in] map The map used for permutation
-template <typename IntType, std::size_t DIM>
-bool is_transpose_needed(const std::array<IntType, DIM>& map) {
-  static_assert(std::is_integral_v<IntType>,
-                "is_transpose_needed: IntType must be an integral type.");
-  std::array<IntType, DIM> contiguous_map;
+template <typename IndexType, std::size_t DIM>
+bool is_transpose_needed(const std::array<IndexType, DIM>& map) {
+  static_assert(std::is_integral_v<IndexType>,
+                "is_transpose_needed: IndexType must be an integral type.");
+  std::array<IndexType, DIM> contiguous_map;
   std::iota(contiguous_map.begin(), contiguous_map.end(), 0);
   return map != contiguous_map;
 }
