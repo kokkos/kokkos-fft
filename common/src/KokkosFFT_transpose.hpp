7718ea386da1d35770696bc24a62031cb21580fc
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2025-11-07 23:46:15 +0900

    improve iteration pattern in transpose

--- common/src/KokkosFFT_transpose.hpp
+++ common/src/KokkosFFT_transpose.hpp
@@ -172,9 +172,14 @@ struct Transpose {
           Kokkos::RangePolicy<ExecutionSpace, Kokkos::IndexType<IndexType>>;
       return range_policy_type(space, 0, x.extent(0));
     } else {
+      using LayoutType = typename InViewType::array_layout;
+      static const Kokkos::Iterate outer_iteration_pattern =
+          layout_iterate_type_selector<LayoutType>::outer_iteration_pattern;
+      static const Kokkos::Iterate inner_iteration_pattern =
+          layout_iterate_type_selector<LayoutType>::inner_iteration_pattern;
       using iterate_type =
-          Kokkos::Rank<m_rank_truncated, Kokkos::Iterate::Default,
-                       Kokkos::Iterate::Default>;
+          Kokkos::Rank<m_rank_truncated, outer_iteration_pattern,
+                       inner_iteration_pattern>;
       using mdrange_policy_type =
           Kokkos::MDRangePolicy<ExecutionSpace, iterate_type,
                                 Kokkos::IndexType<IndexType>>;
