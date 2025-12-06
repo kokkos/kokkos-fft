24753e9641ff17e9985a29b13f6c12fbe133de0e
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2025-12-07 03:15:35 +0900

    Add dynamic plan for SYCL

--- fft/src/KokkosFFT_SYCL_transform.hpp
+++ fft/src/KokkosFFT_SYCL_transform.hpp
@@ -16,62 +16,62 @@
 namespace KokkosFFT {
 namespace Impl {
 template <typename PlanType>
-void exec_plan(PlanType& plan, float* idata, std::complex<float>* odata,
+void exec_plan(const PlanType& plan, float* idata, std::complex<float>* odata,
                int /*direction*/) {
   Kokkos::Profiling::ScopedRegion region(
       "KokkosFFT::exec_plan[TPL_oneMKLExecR2C]");
-  oneapi::mkl::dft::compute_forward(plan, idata,
+  oneapi::mkl::dft::compute_forward(plan.plan(), idata,
                                     reinterpret_cast<float*>(odata));
 }
 
 template <typename PlanType>
-void exec_plan(PlanType& plan, double* idata, std::complex<double>* odata,
+void exec_plan(const PlanType& plan, double* idata, std::complex<double>* odata,
                int /*direction*/) {
   Kokkos::Profiling::ScopedRegion region(
       "KokkosFFT::exec_plan[TPL_oneMKLExecD2Z]");
-  oneapi::mkl::dft::compute_forward(plan, idata,
+  oneapi::mkl::dft::compute_forward(plan.plan(), idata,
                                     reinterpret_cast<double*>(odata));
 }
 
 template <typename PlanType>
-void exec_plan(PlanType& plan, std::complex<float>* idata, float* odata,
+void exec_plan(const PlanType& plan, std::complex<float>* idata, float* odata,
                int /*direction*/) {
   Kokkos::Profiling::ScopedRegion region(
       "KokkosFFT::exec_plan[TPL_oneMKLExecC2R]");
-  oneapi::mkl::dft::compute_backward(plan, reinterpret_cast<float*>(idata),
-                                     odata);
+  oneapi::mkl::dft::compute_backward(plan.plan(),
+                                     reinterpret_cast<float*>(idata), odata);
 }
 
 template <typename PlanType>
-void exec_plan(PlanType& plan, std::complex<double>* idata, double* odata,
+void exec_plan(const PlanType& plan, std::complex<double>* idata, double* odata,
                int /*direction*/) {
   Kokkos::Profiling::ScopedRegion region(
       "KokkosFFT::exec_plan[TPL_oneMKLExecZ2D]");
-  oneapi::mkl::dft::compute_backward(plan, reinterpret_cast<double*>(idata),
-                                     odata);
+  oneapi::mkl::dft::compute_backward(plan.plan(),
+                                     reinterpret_cast<double*>(idata), odata);
 }
 
 template <typename PlanType>
-void exec_plan(PlanType& plan, std::complex<float>* idata,
+void exec_plan(const PlanType& plan, std::complex<float>* idata,
                std::complex<float>* odata, int direction) {
   Kokkos::Profiling::ScopedRegion region(
       "KokkosFFT::exec_plan[TPL_oneMKLExecC2C]");
   if (direction == 1) {
-    oneapi::mkl::dft::compute_forward(plan, idata, odata);
+    oneapi::mkl::dft::compute_forward(plan.plan(), idata, odata);
   } else {
-    oneapi::mkl::dft::compute_backward(plan, idata, odata);
+    oneapi::mkl::dft::compute_backward(plan.plan(), idata, odata);
   }
 }
 
 template <typename PlanType>
-void exec_plan(PlanType& plan, std::complex<double>* idata,
+void exec_plan(const PlanType& plan, std::complex<double>* idata,
                std::complex<double>* odata, int direction) {
   Kokkos::Profiling::ScopedRegion region(
       "KokkosFFT::exec_plan[TPL_oneMKLExecZ2Z]");
   if (direction == 1) {
-    oneapi::mkl::dft::compute_forward(plan, idata, odata);
+    oneapi::mkl::dft::compute_forward(plan.plan(), idata, odata);
   } else {
-    oneapi::mkl::dft::compute_backward(plan, idata, odata);
+    oneapi::mkl::dft::compute_backward(plan.plan(), idata, odata);
   }
 }
 }  // namespace Impl
