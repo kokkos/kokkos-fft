d3d77c372736f03a59534041871cc14143da2285
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2025-12-09 17:52:00 +0900

    make  plan getter const

--- fft/src/KokkosFFT_SYCL_types.hpp
+++ fft/src/KokkosFFT_SYCL_types.hpp
@@ -110,7 +110,7 @@ struct ScopedoneMKLPlan {
   ScopedoneMKLPlan &operator=(ScopedoneMKLPlan &&)      = delete;
   ScopedoneMKLPlan(ScopedoneMKLPlan &&)                 = delete;
 
-  onemklHandle &plan() noexcept { return m_plan; }
+  const onemklHandle &plan() const noexcept { return m_plan; }
 
   void use_external_workspace() {
     // Use externally allocated workspaces
