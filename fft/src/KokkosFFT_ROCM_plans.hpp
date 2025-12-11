3cbc5e0f51db8102d7589be151f15c35c22c8a40
yasahi-hpc <57478230+yasahi-hpc@users.noreply.github.com>
2025-12-11 14:56:38 +0100

    We now have all the backends (#372)
    Co-authored-by: Yuuichi Asahi <y.asahi@nr.titech.ac.jp>

--- fft/src/KokkosFFT_ROCM_plans.hpp
+++ fft/src/KokkosFFT_ROCM_plans.hpp
@@ -76,6 +76,42 @@ auto create_plan(const ExecutionSpace& exec_space,
   plan = std::make_unique<PlanType>(type, in_extents, out_extents, fft_extents,
                                     howmany, direction, is_inplace);
   plan->commit(exec_space);
+  plan->set_work_area();
+
+  // Calculate the total size of the FFT
+  int fft_size = total_size(fft_extents);
+
+  return fft_size;
+}
+
+// batched transform, over ND Views
+template <typename ExecutionSpace, typename PlanType, typename InViewType,
+          typename OutViewType,
+          std::enable_if_t<std::is_same_v<ExecutionSpace, Kokkos::HIP>,
+                           std::nullptr_t> = nullptr>
+auto create_dynplan(const ExecutionSpace& exec_space,
+                    std::unique_ptr<PlanType>& plan, const InViewType& in,
+                    const OutViewType& out, Direction direction,
+                    std::size_t dim, bool is_inplace) {
+  KOKKOSFFT_STATIC_ASSERT_VIEWS_ARE_OPERATABLE(
+      (KokkosFFT::Impl::are_operatable_views_v<ExecutionSpace, InViewType,
+                                               OutViewType>),
+      "create_dynplan");
+  using in_value_type  = typename InViewType::non_const_value_type;
+  using out_value_type = typename OutViewType::non_const_value_type;
+
+  Kokkos::Profiling::ScopedRegion region(
+      "KokkosFFT::create_dynplan[TPL_rocfft]");
+  constexpr auto type =
+      KokkosFFT::Impl::transform_type<ExecutionSpace, in_value_type,
+                                      out_value_type>::type();
+  auto [in_extents, out_extents, fft_extents, howmany] =
+      KokkosFFT::Impl::get_extents(in, out, dim, is_inplace);
+
+  // Create a plan
+  plan = std::make_unique<PlanType>(type, in_extents, out_extents, fft_extents,
+                                    howmany, direction, is_inplace);
+  plan->commit(exec_space);
 
   // Calculate the total size of the FFT
   int fft_size = total_size(fft_extents);
