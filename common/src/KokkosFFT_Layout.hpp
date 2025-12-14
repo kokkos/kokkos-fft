2329eb9c5318688f26ca98e0763dc6c2b0c2a2f3
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2025-12-15 00:46:30 +0900

    Add unit-test for Layout

---
+++ common/src/KokkosFFT_Layout.hpp
@@ -0,0 +1,61 @@
+// SPDX-FileCopyrightText: (C) The kokkos-fft development team, see COPYRIGHT.md file
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0 WITH LLVM-exception
+
+#ifndef KOKKOSFFT_LAYOUT_HPP
+#define KOKKOSFFT_LAYOUT_HPP
+
+#include <type_traits>
+#include <numeric>
+#include <Kokkos_Core.hpp>
+
+namespace KokkosFFT {
+namespace Impl {
+
+// Copied from Kokkos_Layout.hpp
+// Since this is not publicly exposed, we re-implement it here
+// to avoid dependency on Kokkos implementation details
+template <typename... Layout>
+struct layout_iterate_type_selector {
+  static_assert(true,
+                "layout_iterate_type_selector: Layout must be one of "
+                "LayoutLeft, LayoutRight, LayoutStride");
+};
+
+template <>
+struct layout_iterate_type_selector<Kokkos::LayoutRight> {
+  static constexpr Kokkos::Iterate outer_iteration_pattern = Kokkos::Iterate::Right;
+  static constexpr Kokkos::Iterate inner_iteration_pattern = Kokkos::Iterate::Right;
+};
+
+template <>
+struct layout_iterate_type_selector<Kokkos::LayoutLeft> {
+  static constexpr Kokkos::Iterate outer_iteration_pattern = Kokkos::Iterate::Left;
+  static constexpr Kokkos::Iterate inner_iteration_pattern = Kokkos::Iterate::Left;
+};
+
+template <>
+struct layout_iterate_type_selector<Kokkos::LayoutStride> {
+  static constexpr Kokkos::Iterate outer_iteration_pattern =
+      Kokkos::Iterate::Default;
+  static constexpr Kokkos::Iterate inner_iteration_pattern =
+      Kokkos::Iterate::Default;
+};
+
+template <typename Layout, typename IndexType, std::size_t N>
+Layout create_layout(const std::array<IndexType, N>& extents) {
+  static_assert(std::is_integral_v<IndexType>,
+                "create_layout: IndexType must be an integral type");
+  static_assert(std::is_same_v<Layout, Kokkos::LayoutLeft> ||
+                    std::is_same_v<Layout, Kokkos::LayoutRight>,
+                "create_layout: Layout must be either Kokkos::LayoutLeft or "
+                "Kokkos::LayoutRight.");
+  Layout layout;
+  std::copy_n(extents.begin(), N, layout.dimension);
+  return layout;
+}
+
+}  // namespace Impl
+}  // namespace KokkosFFT
+
+#endif
---
+++ common/unit_test/Test_Layout.cpp
@@ -0,0 +1,96 @@
+// SPDX-FileCopyrightText: (C) The kokkos-fft development team, see COPYRIGHT.md file
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0 WITH LLVM-exception
+
+#include <gtest/gtest.h>
+#include <Kokkos_Core.hpp>
+#include "KokkosFFT_Layout.hpp"
+
+namespace {
+using execution_space = Kokkos::DefaultExecutionSpace;
+
+template <std::size_t DIM>
+using axes_type    = std::array<int, DIM>;
+using layout_types = ::testing::Types<Kokkos::LayoutLeft, Kokkos::LayoutRight,
+                                      Kokkos::LayoutStride>;
+
+using test_types =
+    ::testing::Types<std::pair<int, Kokkos::LayoutLeft>,
+                     std::pair<int, Kokkos::LayoutRight>,
+                     std::pair<std::size_t, Kokkos::LayoutLeft>,
+                     std::pair<std::size_t, Kokkos::LayoutRight> >;
+
+// Basically the same fixtures, used for labeling tests
+template <typename T>
+struct CompileTestLayoutIterate : public ::testing::Test {
+  using layout_type = T;
+
+  virtual void SetUp() {
+    GTEST_SKIP() << "Skipping all tests for this fixture";
+  }
+};
+
+template <typename T>
+struct TestCreateLayout : public ::testing::Test {
+  using index_type  = typename T::first_type;
+  using layout_type = typename T::second_type;
+};
+
+template <typename LayoutType>
+void test_layout_iterate_type_selector() {
+  if constexpr (std::is_same_v<LayoutType, Kokkos::LayoutLeft>) {
+    ASSERT_EQ(KokkosFFT::Impl::layout_iterate_type_selector<
+                  LayoutType>::inner_iteration_pattern,
+              Kokkos::Iterate::Left);
+    ASSERT_EQ(KokkosFFT::Impl::layout_iterate_type_selector<
+                  LayoutType>::outer_iteration_pattern,
+              Kokkos::Iterate::Left);
+  } else if constexpr (std::is_same_v<LayoutType, Kokkos::LayoutRight>) {
+    ASSERT_EQ(KokkosFFT::Impl::layout_iterate_type_selector<
+                  LayoutType>::inner_iteration_pattern,
+              Kokkos::Iterate::Right);
+    ASSERT_EQ(KokkosFFT::Impl::layout_iterate_type_selector<
+                  LayoutType>::outer_iteration_pattern,
+              Kokkos::Iterate::Right);
+  } else {
+    ASSERT_EQ(KokkosFFT::Impl::layout_iterate_type_selector<
+                  LayoutType>::inner_iteration_pattern,
+              Kokkos::Iterate::Default);
+    ASSERT_EQ(KokkosFFT::Impl::layout_iterate_type_selector<
+                  LayoutType>::outer_iteration_pattern,
+              Kokkos::Iterate::Default);
+  }
+}
+
+template <typename IndexType, typename LayoutType>
+void test_create_layout() {
+  constexpr std::size_t max_rank = 8;
+  for (std::size_t rank = 1; rank <= max_rank; ++rank) {
+    std::array<IndexType, max_rank> extents;
+    for (std::size_t i = 0; i < max_rank; ++i) {
+      extents.at(i) = static_cast<IndexType>(i + 1);
+    }
+    auto layout = KokkosFFT::Impl::create_layout<LayoutType>(extents);
+    for (std::size_t i = 0; i < max_rank; ++i) {
+      EXPECT_EQ(layout.dimension[i], extents.at(i));
+    }
+  }
+}
+
+}  // namespace
+
+TYPED_TEST_SUITE(CompileTestLayoutIterate, layout_types);
+TYPED_TEST_SUITE(TestCreateLayout, test_types);
+
+// Tests for layout_iterate_type_selector
+TYPED_TEST(CompileTestLayoutIterate, LayoutIterateTypeSelector) {
+  using layout_type = typename TestFixture::layout_type;
+  test_layout_iterate_type_selector<layout_type>();
+}
+
+// Tests for 1D View
+TYPED_TEST(TestCreateLayout, 1Dto8D) {
+  using index_type  = typename TestFixture::index_type;
+  using layout_type = typename TestFixture::layout_type;
+  test_create_layout<index_type, layout_type>();
+}
