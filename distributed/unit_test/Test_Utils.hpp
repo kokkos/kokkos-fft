190c3b747e153c56006e67a1c8d6bc1a1d47b5b7
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2026-02-10 16:43:46 +0900

    remove allclose helper which has not been used yet

--- distributed/unit_test/Test_Utils.hpp
+++ distributed/unit_test/Test_Utils.hpp
@@ -9,35 +9,6 @@
 #include <algorithm>
 #include <Kokkos_Core.hpp>
 
-template <typename ExecutionSpace, typename AViewType, typename BViewType>
-bool allclose(const ExecutionSpace& exec, const AViewType& a,
-              const BViewType& b, double rtol = 1.e-5, double atol = 1.e-8) {
-  constexpr std::size_t rank = AViewType::rank;
-  for (std::size_t i = 0; i < rank; i++) {
-    assert(a.extent(i) == b.extent(i));
-  }
-  const auto n = a.size();
-
-  auto* ptr_a = a.data();
-  auto* ptr_b = b.data();
-
-  int error = 0;
-  Kokkos::parallel_reduce(
-      "KokkosFFT::Test::allclose",
-      Kokkos::RangePolicy<ExecutionSpace, Kokkos::IndexType<std::size_t>>(exec,
-                                                                          0, n),
-      KOKKOS_LAMBDA(const std::size_t& i, int& err) {
-        auto tmp_a = ptr_a[i];
-        auto tmp_b = ptr_b[i];
-        bool not_close =
-            Kokkos::abs(tmp_a - tmp_b) > (atol + rtol * Kokkos::abs(tmp_b));
-        err += static_cast<int>(not_close);
-      },
-      error);
-
-  return error == 0;
-}
-
 template <typename LayoutType, typename iType, std::size_t Rank>
 inline auto get_buffer_shape(const std::array<iType, Rank>& shape,
                              iType nprocs) {
