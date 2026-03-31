16df4d89a110ee272bb304425a1ef0abde39b45d
Yuuichi Asahi <yuuichi.asahi@cea.fr>
2026-03-31 09:16:31 +0200

    use ScopeGuard to initialize Kokkos

--- examples/docs/docs_fft.cpp
+++ examples/docs/docs_fft.cpp
@@ -10,30 +10,27 @@
 /// x = [1, 2, 3, 4]
 /// x_hat = [10, -2+2j, -2, -2-2j]
 int main(int argc, char* argv[]) {
-  Kokkos::initialize(argc, argv);
-  {
-    using ExecutionSpace = Kokkos::DefaultExecutionSpace;
-    using View1D = Kokkos::View<Kokkos::complex<double>*, ExecutionSpace>;
+  Kokkos::ScopeGuard guard(argc, argv);
+  using ExecutionSpace = Kokkos::DefaultExecutionSpace;
+  using View1D = Kokkos::View<Kokkos::complex<double>*, ExecutionSpace>;
 
-    const int n0 = 4;
+  const int n0 = 4;
 
-    View1D x("x", n0), x_hat("x_hat", n0);
-    auto h_x = Kokkos::create_mirror_view(x);
-    for (int i = 0; i < n0; ++i) {
-      h_x(i) = Kokkos::complex<double>(i + 1);
-    }
-    Kokkos::deep_copy(x, h_x);
-
-    ExecutionSpace exec;
-    KokkosFFT::fft(exec, x, x_hat);
-
-    auto h_x_hat =
-        Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, x_hat);
-    for (int i = 0; i < n0; ++i) {
-      std::cout << h_x_hat(i) << std::endl;
-    }
+  View1D x("x", n0), x_hat("x_hat", n0);
+  auto h_x = Kokkos::create_mirror_view(x);
+  for (int i = 0; i < n0; ++i) {
+    h_x(i) = Kokkos::complex<double>(i + 1);
   }
-  Kokkos::finalize();
+  Kokkos::deep_copy(x, h_x);
+
+  ExecutionSpace exec;
+  KokkosFFT::fft(exec, x, x_hat);
+
+  auto h_x_hat =
+      Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, x_hat);
+  for (int i = 0; i < n0; ++i) {
+    std::cout << h_x_hat(i) << std::endl;
+  }
 
   return 0;
 }
