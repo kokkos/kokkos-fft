c5867a2cfc9580352728c7f0ef7c4a9fccfd9f5a
yasahi-hpc <57478230+yasahi-hpc@users.noreply.github.com>
2026-04-05 09:28:00 +0200

    Add ifft() example in API reference (#427)
    * Add ifft() example in API reference
    
    * format
    
    ---------
    
    Co-authored-by: Yuuichi Asahi <y.asahi@nr.titech.ac.jp>

--- examples/docs/docs_ifft.cpp
+++ examples/docs/docs_ifft.cpp
@@ -7,9 +7,9 @@
 #include <Kokkos_Complex.hpp>
 #include <KokkosFFT.hpp>
 
-/// \brief Example of fft usage in documentation
-/// x = [1, 2, 3, 4]
-/// x_hat = [10, -2+2j, -2, -2-2j]
+/// \brief Example of ifft usage in documentation
+/// x = [10, -2+2j, -2, -2-2j]
+/// x_hat = [1, 2, 3, 4]
 int main(int argc, char* argv[]) {
   Kokkos::ScopeGuard guard(argc, argv);
   using ExecutionSpace = Kokkos::DefaultExecutionSpace;
@@ -19,13 +19,14 @@ int main(int argc, char* argv[]) {
 
   View1D x("x", n0), x_hat("x_hat", n0);
   auto h_x = Kokkos::create_mirror_view(x);
-  for (int i = 0; i < n0; ++i) {
-    h_x(i) = Kokkos::complex<double>(i + 1);
-  }
+  h_x(0)   = Kokkos::complex<double>(10, 0);
+  h_x(1)   = Kokkos::complex<double>(-2, 2);
+  h_x(2)   = Kokkos::complex<double>(-2, 0);
+  h_x(3)   = Kokkos::complex<double>(-2, -2);
   Kokkos::deep_copy(x, h_x);
 
   ExecutionSpace exec;
-  KokkosFFT::fft(exec, x, x_hat);
+  KokkosFFT::ifft(exec, x, x_hat);
 
   auto h_x_hat =
       Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, x_hat);
