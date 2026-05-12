3f65e32f949f5c3fcd7ba350139a7c581be84ffb
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2026-05-12 23:15:12 +0900

    fix: .cmake-format.py

--- .cmake-format.py
+++ .cmake-format.py
@@ -7,22 +7,22 @@
 # -----------------------------
 with section("format"):
 
-# How wide to allow formatted cmake files
-line_width = 120
-
-# If an argument group contains more than this many sub-groups (parg or kwarg
-# groups) then force it to a vertical layout.
-max_subgroups_hwrap = 3
-
-# If a statement is wrapped to more than one line, than dangle the closing
-# parenthesis on its own line.
-dangle_parens = True
-
-# If the trailing parenthesis must be 'dangled' on its on line, then align it
-# to this reference: `prefix`: the start of the statement,  `prefix-indent`:
-# the start of the statement, plus one indentation  level, `child`: align to
-# the column of the arguments
-dangle_align = 'prefix'
+    # How wide to allow formatted cmake files
+    line_width = 120
+    
+    # If an argument group contains more than this many sub-groups (parg or kwarg
+    # groups) then force it to a vertical layout.
+    max_subgroups_hwrap = 3
+    
+    # If a statement is wrapped to more than one line, than dangle the closing
+    # parenthesis on its own line.
+    dangle_parens = True
+    
+    # If the trailing parenthesis must be 'dangled' on its on line, then align it
+    # to this reference: `prefix`: the start of the statement,  `prefix-indent`:
+    # the start of the statement, plus one indentation  level, `child`: align to
+    # the column of the arguments
+    dangle_align = 'prefix'
 
 # ------------------------------------------------
 # Options affecting comment reflow and formatting.
