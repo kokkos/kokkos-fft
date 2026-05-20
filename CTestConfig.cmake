75591e5757f28a1fca82d242a4a3b8ede146b6c0
yasahi-hpc <57478230+yasahi-hpc@users.noreply.github.com>
2026-05-20 13:48:16 +0200

    Update CTEST_SUBMIT_URL for submission endpoint

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -4,5 +4,5 @@
 
 set(CTEST_PROJECT_NAME "Kokkos-FFT")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
-set(CTEST_SUBMIT_URL https://my.cdash.org/index.php?project=Kokkos-FFT)
+set(CTEST_SUBMIT_URL https://my.cdash.org/submit.php?project=Kokkos-FFT)
 set(CTEST_DROP_SITE_CDASH TRUE)
