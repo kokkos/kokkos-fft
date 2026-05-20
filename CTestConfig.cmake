249d512989003cdd7aee8abf89bc90a1c0e1f04c
yasahi-hpc <57478230+yasahi-hpc@users.noreply.github.com>
2026-05-20 14:18:34 +0200

    Change nightly start time to midnight UTC
    Set CTEST_NIGHTLY_START_TIME earlier than workflow starts

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -3,6 +3,6 @@
 # SPDX-License-Identifier: MIT OR Apache-2.0 WITH LLVM-exception
 
 set(CTEST_PROJECT_NAME "Kokkos-FFT")
-set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
 set(CTEST_SUBMIT_URL https://my.cdash.org/submit.php?project=Kokkos-FFT)
 set(CTEST_DROP_SITE_CDASH TRUE)
