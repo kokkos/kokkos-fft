578e06ad17b9e71ba84783b5379d95a99773ebb9
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2026-05-20 20:33:15 +0900

    Embed license

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: (C) The Kokkos-FFT development team, see COPYRIGHT.md file
+#
+# SPDX-License-Identifier: MIT OR Apache-2.0 WITH LLVM-exception
+
 set(CTEST_PROJECT_NAME Kokkos FFT)
 set(CTEST_NIGHTLY_START_TIME 01:00:00 UTC)
 set(CTEST_SUBMIT_URL https://my.cdash.org/index.php?project=Kokkos-FFT)
