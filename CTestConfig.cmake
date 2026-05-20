327da10789ffb2e0ae3e9190b856da008cf19be3
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2026-05-20 20:29:17 +0900

    Attempt to add nightly CI on ruche

---
+++ CTestConfig.cmake
@@ -0,0 +1,4 @@
+set(CTEST_PROJECT_NAME Kokkos FFT)
+set(CTEST_NIGHTLY_START_TIME 01:00:00 UTC)
+set(CTEST_SUBMIT_URL https://my.cdash.org/index.php?project=Kokkos-FFT)
+set(CTEST_DROP_SITE_CDASH TRUE)
