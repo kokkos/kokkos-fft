e7dd5e4b056e58dbd10958de170894a32071efde
yasahi-hpc <57478230+yasahi-hpc@users.noreply.github.com>
2026-05-26 21:37:59 +0200

    Publish hpsf CI on CDASH (#478)
    * Publish hpsf CI on CDASH
    
    * whitelist HSA_XNACK
    
    * fix: CMake format
    
    * Add HSA_XNACK under extend-identifiers
    
    * Remove GITHUB_PR branch as this is nightly only
    
    * Remove unused flags and fix CTestRun.cmake path in .gitlab-ci.yml
    
    ---------
    
    Co-authored-by: Yuuichi Asahi <y.asahi@nr.titech.ac.jp>

---
+++ .gitlab/CTestRun.cmake
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: (C) The Kokkos-FFT development team, see COPYRIGHT.md file
+#
+# SPDX-License-Identifier: MIT OR Apache-2.0 WITH LLVM-exception
+
+cmake_minimum_required(VERSION 3.22)
+
+set(CTEST_BINARY_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/build)
+set(CTEST_SOURCE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+
+set(CTEST_UPDATE_COMMAND git)
+set(CTEST_UPDATE_VERSION_ONLY 1)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+
+file(REMOVE_RECURSE "${CTEST_BINARY_DIRECTORY}")
+
+ctest_start(${CDASH_MODEL})
+ctest_update()
+ctest_configure(OPTIONS "${CMAKE_OPTIONS}" RETURN_VALUE config_result)
+ctest_build(RETURN_VALUE build_result)
+
+# Only run tests if the build was successful
+if(NOT build_result)
+  ctest_test(RETURN_VALUE test_result)
+endif()
+
+# Always submit results, even if there were errors
+ctest_submit()
+
+if(config_result)
+  message(FATAL_ERROR "Error during configuration! Exit code: ${config_result}")
+endif()
+if(build_result)
+  message(FATAL_ERROR "Error during build! Exit code: ${build_result}")
+endif()
+if(test_result)
+  message(FATAL_ERROR "Error during testing! Exit code: ${test_result}")
+endif()
