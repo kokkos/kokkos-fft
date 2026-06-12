731ddfd3c08d5643a1bae553fb0ec498dc969977
yasahi-hpc <57478230+yasahi-hpc@users.noreply.github.com>
2026-06-12 11:31:52 +0200

    [Docs] Add hfft/ihfft example (#488)
    * Add hfft/ihfft examples
    
    * update CMake for examples
    
    * Add documentation for hfft and ihfft
    
    * fixed the data shape based on review
    
    * fix: docs and examples
    
    ---------
    
    Co-authored-by: Yuuichi Asahi <y.asahi@nr.titech.ac.jp>

---
+++ examples/docs/hermitian/docs_hfft.cpp
@@ -0,0 +1,38 @@
+// SPDX-FileCopyrightText: (C) The Kokkos-FFT development team, see COPYRIGHT.md file
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0 WITH LLVM-exception
+
+#include <iostream>
+#include <Kokkos_Core.hpp>
+#include <Kokkos_Complex.hpp>
+#include <KokkosFFT.hpp>
+
+/// \brief Example of hfft usage in documentation
+/// x = [1, 2, 3, 4]
+/// x_hat = [15.0, -4.0, 0.0, -1.0, 0.0, -4.0]
+int main(int argc, char* argv[]) {
+  Kokkos::ScopeGuard guard(argc, argv);
+  using ExecutionSpace = Kokkos::DefaultExecutionSpace;
+  using View1D         = Kokkos::View<double*, ExecutionSpace>;
+
+  const int n0 = 6;
+
+  View1D x("x", n0 / 2 + 1), x_hat("x_hat", n0);
+  auto h_x = Kokkos::create_mirror_view(x);
+  for (int i = 0; i < x.extent_int(0); ++i) {
+    h_x(i) = i + 1;
+  }
+  Kokkos::deep_copy(x, h_x);
+
+  ExecutionSpace exec;
+  KokkosFFT::hfft(exec, x, x_hat);
+
+  auto h_x_hat =
+      Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, x_hat);
+  for (int i = 0; i < x_hat.extent_int(0); ++i) {
+    std::cout << " " << h_x_hat(i);
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
