d6539e96f4cbc40aba7985425f39e310547b2c3c
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2026-06-24 14:24:08 +0900

    Add fftshift/ifftshift examples

--- docs/api/helper/ifftshift.rst
+++ docs/api/helper/ifftshift.rst
@@ -7,3 +7,17 @@ KokkosFFT::ifftshift
 
 .. doxygenfunction:: KokkosFFT::ifftshift(const ExecutionSpace& exec_space, const ViewType& inout, std::optional<int> axes = std::nullopt)
 .. doxygenfunction:: KokkosFFT::ifftshift(const ExecutionSpace& exec_space, const ViewType& inout, axis_type<DIM> axes)
+
+Examples
+========
+
+.. literalinclude:: ../../../examples/docs/helper/docs_ifftshift.cpp
+  :language: c++
+  :linenos:
+  :lines: 5-
+
+Expected output:
+
+.. code::
+
+ 0 1 2 3 4 -4 -3 -2 -1
---
+++ examples/docs/helper/docs_fftshift.cpp
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: (C) The Kokkos-FFT development team, see COPYRIGHT.md file
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0 WITH LLVM-exception
+
+#include <iostream>
+#include <Kokkos_Core.hpp>
+#include <Kokkos_Complex.hpp>
+#include <KokkosFFT.hpp>
+
+/// \brief Example of fftshift usage in documentation
+/// freqs = KokkosFFT::fftfreq(10, 0.1);
+/// freqs: [0 1 2 3 4 5 -4 -3 -2 -1]
+/// KokkosFFT::fftshift(freqs);
+/// freqs_shifted: [-5 -4 -3 -2 -1 0 1 2 3 4]
+int main(int argc, char* argv[]) {
+  Kokkos::ScopeGuard guard(argc, argv);
+  using ExecutionSpace = Kokkos::DefaultExecutionSpace;
+
+  const int n0 = 10;
+  ExecutionSpace exec;
+  auto freq = KokkosFFT::fftfreq<ExecutionSpace, double>(exec, n0, 0.1);
+  KokkosFFT::fftshift(exec, freq);
+  auto h_freq = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, freq);
+  for (int i = 0; i < freq.extent_int(0); ++i) {
+    std::cout << " " << h_freq(i);
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
